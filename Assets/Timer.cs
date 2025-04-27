4f9a1021a471e731651c7cdb322bf0b54c79d023
Itsguo14 <maagaardlarsen@gmail.com>
2025-04-27 13:15:02 +0200

    Timer and countdown

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    float elapsedTime;
+    [SerializeField] TextMeshProUGUI countdownText; // Add a separate TextMeshProUGUI for the countdown
+    private float elapsedTime;
+    private float countdownTime = 5f; // Countdown starts from 5 seconds
+    private bool isCountdownComplete = false;
 
-    // Update is called once per frame
+    void Start()
+    {
+        // Initialize the countdown text
+        countdownText.gameObject.SetActive(true);
+        timerText.gameObject.SetActive(false);
+    }
+
     void Update()
     {
-        elapsedTime+=Time.deltaTime;
+        if (!isCountdownComplete)
+        {
+            HandleCountdown();
+        }
+        else
+        {
+            HandleTimer();
+        }
+    }
+
+    private void HandleCountdown()
+    {
+        countdownTime -= Time.deltaTime;
+        int seconds = Mathf.CeilToInt(countdownTime);
+        countdownText.text = seconds.ToString();
+
+        if (countdownTime <= 0)
+        {
+            isCountdownComplete = true;
+            countdownText.gameObject.SetActive(false);
+            timerText.gameObject.SetActive(true);
+        }
+    }
+
+    private void HandleTimer()
+    {
+        elapsedTime += Time.deltaTime;
         int minutes = Mathf.FloorToInt(elapsedTime / 60);
         int seconds = Mathf.FloorToInt(elapsedTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
